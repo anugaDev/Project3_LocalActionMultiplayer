a0f78bf6e5d2a548817ab76b26d7cb93b3ec76cd
1boker1 <nil.ll.lopez@gmail.com>
2019-04-08 19:32:24 +0200

    IT'S COMING HOME

---
+++ Catch&ThrowProject/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private StateMachine stateMachine = new StateMachine();
+
+    private void Update()
+    {
+        stateMachine.ExecuteState();
+    }
+}
---
+++ Catch&ThrowProject/Assets/Scripts/States/StateMachine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public BaseState currentState;
+
+    public void ChangeState(BaseState nextState)
+    {
+        if (currentState != null) currentState.Exit();
+
+        currentState = nextState;
+        currentState.Enter();
+    }
+
+    public void ExecuteState()
+    {
+        if (currentState != null) currentState.Execute();
+    }
+}
