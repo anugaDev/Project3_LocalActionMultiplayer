ccaaf974b735733606191de592be922df4644277
1boker1 <nil.ll.lopez@gmail.com>
2019-04-09 14:14:49 +0200

    PlayerInput started

--- Catch&ThrowProject/Assets/Scripts/Character/PlayerController.cs
+++ Catch&ThrowProject/Assets/Scripts/Character/PlayerController.cs
@@ -8,15 +8,15 @@ public class PlayerController : MonoBehaviour
     [Header("Components")]
     public Animator animator;
     public Rigidbody rigidbody;
-    
+
     [Header("Classes")]
-    
+
     [HideInInspector] public StateMachine stateMachine = new StateMachine();
     public Shield shield;
     public InputController inputControl;
-    
+
     [Header("States")]
-    
+
     public Idle idleState;
     public Walk walkState;
     public Jump jumpState;
