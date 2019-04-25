e4af3251fb268266bdb49a697874c5493eff2078
adrinuga <38584756+adrinuga@users.noreply.github.com>
2019-04-25 17:47:03 +0200

    TrainPrefab

---
+++ Catch&ThrowProject/Assets/Scripts/Utilities/GameUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUtilities : MonoBehaviour
+{
+    public IEnumerator ShakeObject(float time, Transform objectToShake, float force)
+    {
+        var originalPos = objectToShake.position;
+        var actualTime = 0f;
+        
+        while (actualTime < time)
+        {
+            var randomUnity = Random.insideUnitSphere;
+            randomUnity.z = 0;
+            objectToShake.position = originalPos + (randomUnity * force);
+            yield return null;
+            actualTime+= Time.deltaTime;
+        }
+        objectToShake.position = originalPos;
+    }
+
+    public IEnumerator ScaleTime(float time)
+    {
+        yield return null;
+    }
+}
